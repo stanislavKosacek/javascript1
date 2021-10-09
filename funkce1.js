c818fbf75260feb6c5e82d357f9ad827747e866f
stanislavKosacek <stanislav.kosacek@icloud.com>
2021-10-09 14:58:20 +0200

    1 file modified, 1 added

---
+++ funkce1.js
@@ -0,0 +1,12 @@
+function pocitej(a, b) {
+  let soucet = a + b;
+  let rozdil = a - b;
+  let soucin = a * b;
+  let podil = a / b;
+  console.log('Součet:', soucet);
+  console.log('Rozdíl:', rozdil);
+  console.log('Součin:', soucin);
+  console.log('Podíl:', podil);
+}
+
+pocitej(10, 20);
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // Import stylesheets
-import './cyklus3.js';
+import './funkce1.js';
