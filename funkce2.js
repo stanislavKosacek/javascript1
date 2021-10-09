b73b6663e038ec14e6fccf9d9f17eae67eea5117
stanislavKosacek <stanislav.kosacek@icloud.com>
2021-10-09 15:23:11 +0200

    1 file modified

--- funkce2.js
+++ funkce2.js
@@ -2,3 +2,21 @@
 // - prvočíslo - dělitelné pouze 1 a samo sebou​
 //  zbytek po dělení: % - modulo​
 // - v prvni moment predpokladejme, ze cislo JE prvocislo a budeme pripadne rosporovat toto tvrzeni.
+
+function jePrvocislo(cislo) {
+  for (let i = 2; i < cislo; i++) {
+    if (cislo % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let vstup = Number(prompt('Zadej číslo:'));
+
+if (jePrvocislo(vstup)) {
+  console.log('Ano, číslo ' + vstup + ' je prvočíslo.');
+} else {
+  console.log('Ne, číslo ' + vstup + ' není provočíslo.');
+}
